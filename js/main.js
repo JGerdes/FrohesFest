dcc5cd4b9214a56f2647f1e6712b210c83746a4c
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-12-05 16:33:26 +0100

    polygon test

--- js/main.js
+++ js/main.js
@@ -12,12 +12,33 @@ function preload() {
 function create() {
 	game.add.sprite(0, 0, 'background_light');
 
-	frontTrees = game.add.group();
-	for(var i=0; i<10; i++){
-		frontTrees.create(i*256, game.world.height - 256, 'tree01');
+	points = {
+	'x': [ 0, 200, 600, 1300 ],
+	'y': [ 440, 210, 250, 100 ]
+	};
+ 
+	bmd = game.add.bitmapData(game.world.width, game.world.height);
+	bmd.addToWorld();
+
+	bmd.ctx.fillStyle = '#f4f4f4';
+	bmd.ctx.beginPath();
+	bmd.ctx.moveTo(0, game.world.height);
+	for(var i=0; i<game.world.width; i++){
+		var index = i/game.world.width;
+		var x = game.math.catmullRomInterpolation(points.x, index);
+		var y = game.world.height - game.math.catmullRomInterpolation(points.y, index);
+		bmd.ctx.lineTo(x,y);
 	}
 
-	
+	bmd.ctx.lineTo(game.world.width, game.world.height);
+	bmd.ctx.closePath();
+	bmd.ctx.fill();
+
+
+	frontTrees = game.add.group();
+	for(var i=0; i<10; i++){
+		frontTrees.create(i*512, game.world.height - 256, 'tree01');
+	}
 	 
 }
 
