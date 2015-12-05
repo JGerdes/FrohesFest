4e11b50310cbf3bfc09be584f001c87dd14adcbf
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-12-05 20:13:58 +0100

    parallax mountains

--- js/main.js
+++ js/main.js
@@ -2,17 +2,39 @@
 
 var game = new Phaser.Game(1280, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-var frontTrees, sledge;
+var mountainRow1, mountainRow2, frontTrees, sledge;
 
 function preload() {
 	game.load.image('background_light', 'assets/graphics/background_light.png');
 	game.load.image('tree01', 'assets/graphics/tree01.png');
+	game.load.image('mountain01', 'assets/graphics/mountain01.png');
+	game.load.image('mountain02', 'assets/graphics/mountain02.png');
+	game.load.image('mountain03', 'assets/graphics/mountain03.png');
 	game.load.image('sledge', 'assets/graphics/sledge.png');
 }
 
 function create() {
 	game.world.setBounds(-0, 0, 5000, 720);
 	game.add.image(0, 0, 'background_light').fixedToCamera = true;
+
+	mountainRow1 = game.add.group();
+	start = -512;
+	for(var i=0; i<30; i++){
+		start += 256 + Math.random()*192;
+		var mountid = Math.floor(Math.random()* 2) + 2;
+		var temp = mountainRow1.create(start, 640, 'mountain0'+ mountid);
+		temp.position.y -= temp.height; 
+		temp.alpha = 0.6;
+
+	}
+	mountainRow2 = game.add.group();
+	start = -512;
+	for(var i=0; i<30; i++){
+		start += 256 + Math.random()*192;
+		var temp = mountainRow2.create(start, 640, 'mountain01');
+		temp.position.y -= temp.height; 
+
+	}
 
 	points = {
 	'x': [ 0, 200, 600, 1300, 3000, 4800],
@@ -42,7 +64,7 @@ function create() {
 
 
 	frontTrees = game.add.group();
-	var start = 0;
+	start = 0;
 	for(var i=0; i<10; i++){
 		start += 512 + Math.random()*1024;
 		frontTrees.create(start, game.world.height - 256, 'tree01');
@@ -90,6 +112,8 @@ function update() {
 		sledge.body.moveRight(500);
 	}
 	frontTrees.x = game.camera.x * - 0.8;
+	mountainRow1.x = game.camera.x * 0.9;
+	mountainRow2.x = game.camera.x * 0.8;
 }
 
 
