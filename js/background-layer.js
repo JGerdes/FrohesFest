586b598886c84e747f7039479baccc3df65fa8da
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-12-06 17:13:24 +0100

    remove fully passed sprites from background

--- js/background-layer.js
+++ js/background-layer.js
@@ -7,14 +7,13 @@ function BackgroundLayer(group, images, minMargin, maxMargin, y, scrollSpeed){
 	this.game = group.game;
 	this.camera = this.game.camera;
 	this.scrollSpeed = scrollSpeed;
+	this.scrollSpeedInv = 1 - scrollSpeed;
 	this.sprites = [];
 	this.lastGenerated = -512;
 
-	this.lastGenerated += 256 + Math.random()*192;
-			var temp = this.group.create(this.lastGenerated, this.y, 'mountain01');
-			temp.position.y -= temp.height; 
-	
-		this.lastGenerated = this.lastGenerated;
+	while(this.camera.view.x * 0.05+ this.camera.view.width - this.minMargin > this.lastGenerated){
+		this.generateSprite();
+	}
 	
 }
 	
@@ -22,13 +21,20 @@ function BackgroundLayer(group, images, minMargin, maxMargin, y, scrollSpeed){
 BackgroundLayer.prototype.update = function(){
 	this.group.x = this.game.camera.x * this.scrollSpeed;
 
-	if(this.camera.view.x * 0.05+ this.camera.view.width - this.minMargin > this.lastGenerated){
-		var image = this.images[Math.floor(Math.random()*this.images.length)];
-			this.lastGenerated += this.minMargin + Math.random()*this.randMargin;
-			var temp = this.group.create(this.lastGenerated, this.y, image);
-			temp.position.y -= temp.height; 
-	
+	if(this.camera.view.x * this.scrollSpeedInv + this.camera.view.width - this.minMargin > this.lastGenerated){
+		this.generateSprite();
 	}
+	if(this.sprites[0].x + this.sprites[0].width < this.camera.view.x * this.scrollSpeedInv){
+		this.group.remove(this.sprites.shift(), true);
+	}
+}
+
+BackgroundLayer.prototype.generateSprite = function(){
+	var image = this.images[Math.floor(Math.random()*this.images.length)];
+	this.lastGenerated += this.minMargin + Math.random()*this.randMargin;
+	var temp = this.group.create(this.lastGenerated, this.y, image);
+	temp.position.y -= temp.height; 
+	this.sprites.push(temp);
 }
 
 BackgroundLayer.prototype.destroy = function(){
