e97a10e3b490237daed8f04fdb0caf30ec255157
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-12-08 20:38:47 +0100

    read sections from seperate file

--- js/section-controller.js
+++ js/section-controller.js
@@ -1,53 +1,65 @@
-function SectionController(worldData, game){
+function SectionController(worldData, game, sections){
 	this.game = game;
+	this.sections = sections;
+	this.currentSectionIndex = 0;
 }
 
 SectionController.prototype.getNextSection = function(layers){
+	var sec = this.sections[this.currentSectionIndex];
+	this.currentSectionIndex++;
+	var segments = [];
+	for(var i=0; i < sec.segments.length; i++){
+		segments.push(new Segment(this.game, sec.segments[i]));
+	}
+
+	function parseY(value) {
+		if(value === "bottom") {
+			return this.game.world.height;
+		}else{
+			return value;
+		}
+	}
+
 	return new Section(
-		[
-			new Segment(this.game, {
-				'x': [ 0, 200, 600, 900],
-				'y': [ 440, 210, 250, 90]
-			}),
-			new Segment(this.game, {
-				'x': [ 900, 1200, 1400],
-				'y': [ 90, 10, 100]
-			}),
-
-			new Segment(this.game, {
-				'x': [ 1400, 2000, 2500, 3000],
-				'y': [ 100, 500, 250, 100]
-			}),
-
-			new Segment(this.game, {
-				'x': [ 3000, 3500, 4000, 4500, 5000, 5500],
-				'y': [ 100, 200, 100, 200, 100, 200]
-			}),
-
-			new Segment(this.game, {
-				'x': [ 5500, 6000, 7500, 8000],
-				'y': [ 200, 300, 50, 300]
-			}),
-
-			new Segment(this.game, {
-				'x': [ 8000, 9500, 10500, 12500],
-				'y': [ 300, 500, 600, 10]
-			}),
-
-			new Segment(this.game, {
-				'x': [ 12500, 15000],
-				'y': [ 10, 100]
-			}),
-
-			new Segment(this.game, {
-				'x': [ 15000, 15500, 16000, 16500, 17000, 17500, 18000, 18500],
-				'y': [ 100, 200, 100, 200, 100, 200, 100, 200]
-			}),
-		],
-		new BackgroundLayer(layers.background1, ['mountain02', 'mountain03'], 256, 512, 640, 0.95),
-		new BackgroundLayer(layers.background2, ['mountain01'], 256, 512, 640, 0.9),
-		new TiledLayer(layers.tiledBackground, 'forest', game.world.height, 0.85),
-		new MiddlegroundLayer(layers.middleground, ['tree02'], 256, 1024, activeSegments, 0),
-		new ForegroundLayer(layers.foreground, ['tree01'], 512, 2048, game.world.height, -0.9, 128)
+		segments,
+		new BackgroundLayer(
+				layers.background1,
+				sec.background1.images,
+				sec.background1.minDistance,
+				sec.background1.maxDistance,
+				parseY(sec.background1.y),
+				sec.background1.scrollSpeed
+			),
+		new BackgroundLayer(
+				layers.background2,
+				sec.background2.images,
+				sec.background2.minDistance,
+				sec.background2.maxDistance,
+				parseY(sec.background2.y),
+				sec.background2.scrollSpeed
+			),
+		new TiledLayer(
+				layers.tiledBackground,
+				sec.tiledLayer.image,
+				parseY(sec.TiledLayer.y),
+				sec.TiledLayer.scrollSpeed
+			),
+		new MiddlegroundLayer(
+				layers.middleground,
+				sec.middleground.image,
+				sec.middleground.minDistance,
+				sec.middleground.maxDistance, 
+				activeSegments, 
+				sec.middleground.scrollSpeed
+			),
+		new ForegroundLayer(
+				layers.background2,
+				sec.background2.images,
+				sec.background2.minDistance,
+				sec.background2.maxDistance,
+				parseY(sec.background2.y),
+				sec.background2.scrollSpeed,
+				sec.foregroundLayer.yVariation
+			)
 	);
 };
---
+++ js/sections.js
@@ -0,0 +1,73 @@
+var sections = [
+	{
+		"segments": [
+			{
+				"x": [0, 200, 600, 900],
+				"y": [440, 210, 250, 90]
+			},
+			{
+				"x": [900, 1200, 1400],
+				"y": [90, 10, 100]
+			},
+			{
+				"x": [1400, 2000, 2500, 3000],
+				"y": [100, 500, 250, 100]
+			},
+			{
+				"x": [3000, 3500, 4000, 4500, 5000, 5500],
+				"y": [100, 200, 100, 200, 100, 200]
+			},
+			{
+				"x": [5500, 6000, 7500, 8000],
+				"y": [200, 300, 50, 300]
+			},
+			{
+				"x": [8000, 9500, 10500, 12500],
+				"y": [300, 500, 600, 10]
+			},
+			{
+				"x": [12500, 15000],
+				"y": [10, 100]
+			},
+			{
+				"x": [15000, 15500, 16000, 16500, 17000, 17500, 18000, 18500],
+				"y": [100, 200, 100, 200, 100, 200, 100, 200]
+			}
+		],
+		"layer": {
+			"background1": {
+				"images": ["mountain02", "mountain03"],
+				"minDistance": 256,
+				"maxDistance": 512,
+				"y": 640,
+				"scrollSpeed": 0.95
+			},
+			"background2": {
+				"images": ["mountain01"],
+				"minDistance": 256,
+				"maxDistance": 512,
+				"y": 640,
+				"scrollSpeed": 0.9
+			},
+			"tiledLayer": {
+				"image": "forest",
+				"y": "bottom",
+				"scrollSpeed": 0.85
+			},
+			"middleground": {
+				"images": ["tree02"],
+				"minDistance": 256,
+				"maxDistance": 1024,
+				"scrollSpeed": 0
+			},
+			"foreground": {
+				"images": ["tree01"],
+				"minDistance": 512,
+				"maxDistance": 2048,
+				"y": "bottom",
+				"scrollSpeed": -0.9,
+				"yVariation": 128
+			}
+		}
+	}
+];
