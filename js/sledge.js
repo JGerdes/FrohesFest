0cd3d2b71dd167ae8540d3f94128aca7ae208bd6
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-12-18 10:55:31 +0100

    only set blurred background on desktop devices

--- js/sledge.js
+++ js/sledge.js
@@ -89,9 +89,11 @@ Sledge.prototype.create = function(){
 };
 
 Sledge.prototype.die = function(){
-	game.canvas.id = "game";
-	stackBlurCanvasRGB(game.canvas.id, 0, 0, game.width, game.height, 12);
-	game.state.states['gameOver'].dataURI = game.canvas.toDataURL();
+	if(game.device.desktop){
+		game.canvas.id = "game";
+		stackBlurCanvasRGB(game.canvas.id, 0, 0, game.width, game.height, 12);
+		game.state.states['gameOver'].dataURI = game.canvas.toDataURL();
+	}
 	game.state.start('gameOver');
 }
 
